56586d7819b68301c5c6ef5f1dc6a98b75c4cce6
Ignacio Arévalo Martín <nachoarevalomartin@gmail.com>
2014-12-18 22:46:51 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,26 @@
-## [Put comments here that describe what your functions do]
-
-makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y) {
-    x<<-y
-    m<<-NULL
-  }
-  get<-function() x
-  setinverse<-function(solve) m<<-solve
-  getinverse<-function() m
-  list(set=set,get=get,
+makeCacheMatrix<-function(x=matrix()) {
+  m<-NULL # m will be our matrix
+          # It is reset to NULL every time we call makeCacheMatrix
+  set<-function(y) {  # Takes an input matrix
+    x<<-y             # Saves that input matrix
+    m<<-NULL          # Resets to NULL when a new object is generated
+  }       
+  get<-function() x   # This function returns the value of the original matrix 
+  setinverse<-function(solve) m<<-solve   # This function is called by cacheSolve() during the first cacheSolve()
+  getinverse<-function() m  # This function will return the cached value to cacheSolve() 
+  list(set=set,get=get,  # This is a list of the internal functions and it is accessed each time makeCacheMatrix() is called 
        setinverse=setinverse,
-       getinverse=getinverse)
+       getinverse=getinverse)  
 }
 
-cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+cacheSolve <- function(x, ...) { #Input x is a matrix created by makeCacheMatrix()
+  m <- x$getinverse()   #Access the matrix and gets its inverse 
+  if(!is.null(m)) {     #If the matrix was already cached...
+    message("getting cached data")  #...it sends that message to the console...
+    return(m) #...and returns the inverse of the given matrix
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+  data <- x$get() #If x$getinverse() returns NULL...
+  m <- solve(data, ...) #...it calculates the inverse...
+  x$setinverse(m) #... and stores it in x
+  m   # Prints the inverse in the console
 }
